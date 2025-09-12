a454ae81621cb70a504a5af84e672d407384dc9a
eeinoeyes <loveourjune@gmail.com>
2025-09-12 15:09:51 +0900

    [feat] 유저/관리자 로그인 페이지 구현
    [feat] 유저/관리자 로그인 페이지 구현

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,10 @@
-import Button from './components/Button/Button'
+// import Button from './components/Button/Button'
+
+import LoginPage from './pages/user/Login/LoginPage'
+import './assets/styles/commons.scss'
 
 function App() {
-   return (
-      <div style={{ display: 'flex', gap: '1rem', padding: '2rem' }}>
-         <Button variant="primary">Primary 버튼</Button>
-         <Button variant="secondary">Secondary 버튼</Button>
-         <Button variant="danger">Danger 버튼</Button>
-      </div>
-   )
+   return <LoginPage />
 }
 
 export default App
