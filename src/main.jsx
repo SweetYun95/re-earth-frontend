bb3a1af8efd8cfa3179c65aa64b8d73928a62041
SweetYun95 <isr01154@naver.com>
2025-09-15 09:33:36 +0900

    [fix-feat]app폴더 경로로 store 이동, rootReducer.js 추가

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import { createRoot } from 'react-dom/client'
 import { Provider } from 'react-redux'
 import { BrowserRouter } from 'react-router-dom'
 
-import store from './store/store.js'
+import store from './app/store.js'
 import App from './App.jsx'
 
 import 'bootstrap/dist/css/bootstrap.min.css'
