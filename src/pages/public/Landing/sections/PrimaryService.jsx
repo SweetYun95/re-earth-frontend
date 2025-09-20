a599566003b151519aa873a04b03bd015cb4bbec
SweetYun95 <isr01154@naver.com>
2025-09-20 16:46:30 +0900

    [refactor]로그인 폼 통합

--- src/pages/public/Landing/sections/PrimaryService.jsx
+++ src/pages/public/Landing/sections/PrimaryService.jsx
@@ -1,7 +1,7 @@
-import React from 'react'
-import './PrinmaryService.scss' // 파일명이 'Primary'가 아니라 'Prinmary'면 그대로 두세요.
+// re-earth-frontend/src/pages/public/Landing/sections/PrimaryService.jsx
+import './PrinmaryService.scss'
 
-// 이미지 임포트 (경로/파일명은 실제 프로젝트에 맞게 유지)
+// 이미지 임포트
 import card1 from '../../../../assets/images/randingpage(card2) (1).svg'
 import card2 from '../../../../assets/images/randingpage(card2) (2).svg'
 import card3 from '../../../../assets/images/randingpage(card2) (3).svg'
@@ -19,7 +19,6 @@ export default function PrimaryService() {
          </section>
 
          <section className="ps-wrapper">
-            {/* 오른쪽 정렬을 위한 컨테이너 */}
             <div class="container">
                <div class="service-text1">이렇게 해결할 수 있어요.</div>
                <div class="service-text2">Primary Service</div>
